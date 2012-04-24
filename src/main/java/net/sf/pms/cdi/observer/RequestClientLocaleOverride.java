b45ac0ea7a899233bdb2511edc852588f902b2e5
Ivo Maixner <ivo.maixner@gmail.com>
2012-04-24 10:30:38 +0200

    Override @Client Locale from request.

---
+++ src/main/java/net/sf/pms/cdi/observer/RequestClientLocaleOverride.java
@@ -0,0 +1,38 @@
+package net.sf.pms.cdi.observer;
+
+import java.util.Locale;
+
+import javax.enterprise.event.Event;
+import javax.enterprise.event.Observes;
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.inject.Inject;
+
+import org.jboss.seam.faces.event.qualifier.After;
+import org.jboss.seam.faces.event.qualifier.RestoreView;
+import org.jboss.seam.international.Alter;
+import org.jboss.solder.core.Client;
+
+public class RequestClientLocaleOverride {
+
+	@Inject
+	@Client
+	private Locale clientLocale;
+
+	@Inject
+	FacesContext facesContext;
+
+	@Inject
+	@Client
+	@Alter
+	private Event<Locale> alterLocale;
+
+	public void overrideClientLocaleWithRequest(
+			@Observes @After @RestoreView PhaseEvent event) {
+		Locale requestLocale = facesContext.getViewRoot().getLocale();
+		if (!clientLocale.equals(requestLocale)) {
+			alterLocale.fire(requestLocale);
+		}
+	}
+
+}
